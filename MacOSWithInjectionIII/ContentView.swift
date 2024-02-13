b63c0566f812d6752fcc4298eb7d472c57783ed5
Hwee-Boon Yar <hboon@motionobj.com>
2024-02-13 17:06:16 +0800

    Vanila macOS project

---
+++ MacOSWithInjectionIII/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacOSWithInjectionIII
+//
+//  Created by Hwee-Boon Yar on 13/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacOSWithInjectionIII/MacOSWithInjectionIIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSWithInjectionIIIApp.swift
+//  MacOSWithInjectionIII
+//
+//  Created by Hwee-Boon Yar on 13/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MacOSWithInjectionIIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
